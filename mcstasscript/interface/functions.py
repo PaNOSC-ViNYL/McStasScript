a8be979740cf8dc20e7222e15968366b98e3a2b2
mads-bertelsen <mads.bertelsen@gmail.com>
2019-07-26 11:01:55 +0200

    Added a Configurator class to the interface to simplify updating the configuration yaml file. This is in prepration for distribution with pip where the package is installed in a location that will not be easy for everyone to find, and thus allowing them to set configuration from any python terminal is an important feature.
    The code is tested in test_Configurator.py with a number of
    unittests.
    
    The manual is updated to reflect this new method of updating
    the configuration file.

--- mcstasscript/interface/functions.py
+++ mcstasscript/interface/functions.py
@@ -1,3 +1,6 @@
+import yaml
+import os
+
 from mcstasscript.data.data import McStasData
 from mcstasscript.helper.managed_mcrun import ManagedMcrun
 
@@ -91,3 +94,149 @@ def load_data(foldername):
     """
     managed_mcrun = ManagedMcrun("dummy", foldername=foldername)
     return managed_mcrun.load_results()
+
+
+class Configurator:
+    """
+    Class for setting the configuration file for McStasScript.
+    
+    Attributes
+    ----------
+    configuration_file_name : str
+        absolute path of configuration file
+        
+    Methods
+    -------
+    set_mcstas_path(string)
+        sets mcstas path
+        
+    set_mcrun_path(string)
+        sets mcrun path
+        
+    set_line_length(int)
+        sets maximum line length to given int
+    
+    _write_yaml(dict)
+        internal method, writes a configuration yaml file with dict content
+        
+    _read_yaml()
+        internal method, reads a configuration yaml file and returns a dict
+    
+    _create_new_config_file()
+        internal method, creates default configuration file
+
+    """
+
+    def __init__(self, *args):
+        """
+        Initialization of configurator, checks that the configuration file
+        actually exists, and if it does not, creates a default configuration
+        file.
+        
+        Parameters
+        ----------
+        (optional) custom name : str
+            Custom name for configuration file for testing purposes
+        """
+
+        if len(args) == 1:
+            name = args[0]
+        else:
+            name = "configuration"
+
+        # check configuration file exists
+        THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+        self.configuration_file_name = THIS_DIR + "/../" + name + ".yaml"
+        if not os.path.isfile(self.configuration_file_name):
+            # no config file found, write default config file
+            self._create_new_config_file()
+
+    def _write_yaml(self, dictionary):
+        """
+        Writes a dictionary as the new configuration file
+        """
+        with open(self.configuration_file_name, 'w') as yaml_file:
+            yaml.dump(dictionary, yaml_file, default_flow_style=False)   
+
+    def _read_yaml(self):
+        """
+        Reads yaml configuration file
+        """
+        with open(self.configuration_file_name, 'r') as ymlfile:
+            return yaml.safe_load(ymlfile)
+
+    def _create_new_config_file(self):
+        """
+        Writes a default configuration file to the package root directory
+        """
+
+        run = "/Applications/McStas-2.5.app/Contents/Resources/mcstas/2.5/bin/"
+        mcstas = "/Applications/McStas-2.5.app/Contents/Resources/mcstas/2.5/"
+
+        default_paths = {"mcrun_path" : run,
+                         "mcstas_path" : mcstas}
+        default_other = {"characters_per_line" : 93}
+
+        default_config = {"paths" : default_paths,
+                          "other" : default_other}
+
+        self._write_yaml(default_config)
+
+    def set_mcstas_path(self, path):
+        """
+        Sets the path to McStas
+
+        Parameters
+        ----------
+        path : str
+            Path to the mcstas directory containing "sources", "optics", ...
+        """
+
+        # read entire configuration file 
+        config = self._read_yaml()
+
+        # update mcstas_path
+        config["paths"]["mcstas_path"] = path
+
+        # write new configuration file
+        self._write_yaml(config)
+
+    def set_mcrun_path(self, path):
+        """
+        Sets the path to mcrun
+
+        Parameters
+        ----------
+        path : str
+            Path to the mcrun executable
+        """
+
+        # read entire configuration file 
+        config = self._read_yaml()
+
+        # update mcstas_path
+        config["paths"]["mcrun_path"] = path
+
+        # write new configuration file
+        self._write_yaml(config)
+
+    def set_line_length(self, line_length):
+        """
+        Sets maximum line length for output
+
+        Parameters
+        ----------
+        line_length : int
+            maximum line length for output
+        """
+
+        # read entire configuration file 
+        config = self._read_yaml()
+
+        # update mcstas_path
+        config["other"]["characters_per_line"] = int(line_length)
+
+        # write new configuration file
+        self._write_yaml(config)
+
+
