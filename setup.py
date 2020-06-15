163b3005f254ab75104dfc2c5c99b538eed2b06a
Mads Bertelsen <Mads.Bertelsen@ess.eu>
2020-06-15 15:11:43 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='McStasScript',
-     version='0.0.16',
+     version='0.0.17',
      author="Mads Bertelsen",
      author_email="Mads.Bertelsen@ess.eu",
      description="A python scripting interface for McStas",
