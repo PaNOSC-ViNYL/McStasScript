75cd9674eaf9dc0716907d67d537569262625ad0
Mads Bertelsen <mads.bertelsen@ess.eu>
2022-05-18 16:08:37 +0200

    Update of instrument diagram, now makes it as wide as it needs to based on internal connections.

--- mcstasscript/helper/instrument_diagram.py
+++ mcstasscript/helper/instrument_diagram.py
@@ -1,9 +1,11 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import graphviz
 
 def instrument_diagram(instrument):
+    lane_width = 0.033 # spacing between lanes
+    margin = 0.01 # Margin on figure at top and bottom
+
     components = instrument.component_list
     n_components = len(components) + 1
     graph_height = n_components / 2.5
@@ -16,22 +18,22 @@ def instrument_diagram(instrument):
         component_boxes.append(box)
         component_box_dict[component.name] = box
 
-    margin = 0.001
+
     box_height_centers = np.linspace(1-margin, margin, n_components)
     box_height = (1 - 2*margin)/n_components
     for box, y_pos in zip(component_boxes, box_height_centers):
         box.set_y(y_pos)
         box.set_box_height(box_height)
 
-    for box in component_boxes:
-        box.set_x(0.2)
 
-    lane_width = 0.025
 
     arrows = []
-    destinations_AT = set()
-    destinations_ROTATED = set()
     box_names = [x.name for x in component_boxes]
+
+    AT_connections = {}
+    AT_lane_numbers = {}
+    ROTATED_connections = {}
+    ROTATED_lane_numbers = {}
     for component in components:
         if component.AT_reference is None:
             origin = component_box_dict[component.name]
@@ -40,29 +42,7 @@ def instrument_diagram(instrument):
             origin = component_box_dict[component.name]
             target = component_box_dict[component.AT_reference]
 
-        #print(origin.name, "to ", target.name)
-        destinations_AT.add(target.name)
-
-        origin_index = box_names.index(origin.name)
-        target_index = box_names.index(target.name)
-        names_between = box_names[target_index:origin_index]
-        #print(origin_index, target_index)
-        #print("between: ", names_between)
-        unique_destinations_between = destinations_AT.intersection(names_between)
-        lane_number = len(unique_destinations_between)
-        #print("destinations between: ", unique_destinations_between)
-
-        # Different technique for lane numbers:
-
-
-        arrow = Arrow(origin, target)
-        arrow.set_lane_width(lane_width)
-        arrow.set_box_offset_origin(0.24*box_height)
-        arrow.set_box_offset_target(-0.2 * box_height)
-        arrow.set_lane(lane_number)
-
-        arrows.append(arrow)
-        #print()
+        AT_connections[origin] = target
 
         if not component.ROTATED_specified:
             continue
@@ -74,23 +54,52 @@ def instrument_diagram(instrument):
             origin = component_box_dict[component.name]
             target = component_box_dict[component.ROTATED_reference]
 
-        destinations_ROTATED.add(target.name)
+        ROTATED_connections[origin] = target
 
-        origin_index = box_names.index(origin.name)
-        target_index = box_names.index(target.name)
-        names_between = box_names[target_index:origin_index]
-        unique_destinations_between = destinations_ROTATED.intersection(names_between)
+
+    for origin, target in AT_connections.items():
+        lane_number = find_lane_number(origin=origin, target=target, box_names=box_names,
+                                       connections=AT_connections,
+                                       lane_numbers=AT_lane_numbers,
+                                       component_box_dict=component_box_dict)
 
         arrow = Arrow(origin, target)
         arrow.set_lane_width(lane_width)
-        arrow.set_lane_offset(0.5)
+        arrow.set_box_offset_origin(0.24 * box_height)
+        arrow.set_box_offset_target(-0.2 * box_height)
+        arrow.set_lane(lane_number)
+
+        arrows.append(arrow)
+
+    for origin, target in ROTATED_connections.items():
+        lane_number = find_lane_number(origin=origin, target=target, box_names=box_names,
+                                       connections=ROTATED_connections,
+                                       lane_numbers=ROTATED_lane_numbers,
+                                       component_box_dict=component_box_dict)
+
+        arrow = Arrow(origin, target)
+        arrow.set_lane_width(lane_width)
+        arrow.set_lane_offset(0.4)
         arrow.set_box_offset_origin(0.16 * box_height)
         arrow.set_box_offset_target(-0.05 * box_height)
         arrow.color = "red"
-        arrow.set_lane(len(unique_destinations_between))
+
+        arrow.set_lane(lane_number)
         arrows.append(arrow)
 
-    fig, ax = plt.subplots(figsize=(5, graph_height))
+    # Infer how wide the figure should be based on number of lanes used
+    highest_lane = 0
+    for arrow in arrows:
+        highest_lane = max(arrow.lane, highest_lane)
+
+    lane_width = highest_lane/3
+    name_width = 3  # Reserve space of 3 for component names
+    graph_width = lane_width + name_width
+
+    for box in component_boxes:
+        box.set_x(lane_width / graph_width) # Places boxes so they get name_width space
+
+    fig, ax = plt.subplots(figsize=(graph_width, graph_height))
     ax.set(xlim=(0, 1), ylim=(0, 1))
     ax.axis("off")
     for box in component_boxes:
@@ -102,6 +111,30 @@ def instrument_diagram(instrument):
 
     plt.show()
 
+
+def find_lane_number(origin, target, box_names, connections, lane_numbers, component_box_dict):
+    origin_index = box_names.index(origin.name)
+    target_index = box_names.index(target.name)
+    names_between = box_names[target_index + 1:origin_index]
+
+    max_lane_number = 0
+    for name_between in names_between:
+        between_object = component_box_dict[name_between]
+        if name_between == "ABSOLUTE":
+            continue
+
+        if between_object not in connections:
+            continue
+
+        if connections[between_object] is target:
+            continue
+
+        if lane_numbers[between_object] > max_lane_number:
+            max_lane_number = lane_numbers[between_object]
+
+    lane_number = max_lane_number + 1
+    lane_numbers[origin] = lane_number
+    return lane_number
 
 class Component_box:
     def __init__(self, component_object=None, name=None):
@@ -198,24 +231,3 @@ class Arrow:
                  color=self.color, length_includes_head=True,
                  width=self.arrow_width)
 
-
-def instrument_diagram_gv(instrument):
-    dot = graphviz.Digraph('instrument_layout', comment='Connections in instrument', filename="test.gv")
-
-    components = instrument.component_list
-
-    dot.node("ABSOLUTE")
-    for component in components:
-        dot.node(component.name)
-
-    print(dot.source)
-
-    for component in components:
-        if component.AT_reference is None:
-            dot.edge(component.name, "ABSOLUTE", constraint=False)
-        else:
-            dot.edge(component.name, component.AT_reference, constraint=False)
-
-    print(dot.source)
-
-    dot.view()
