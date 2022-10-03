c490e7c1bc748db70b19099d2ac886530cf37aab
Gregory Tucker <gregory.tucker@ess.eu>
2022-10-03 15:46:45 +0200

    [Add] Process return code check for call to mcrun
    - The process.run call to mcrun does not check whether the return code
      has been set to a non-zero value, indicating that an error has
      occurred. This can lead to situations in testing where an error occurs
      but is not detected until later, adding to debugging difficulty.
    - The process.run().returncode is now checked and an error is raised in
      cases where it is non-zero.
    - The call to mcrun should never produce a non-zero value if it has
      succeeded but there may be unintended cases where it returns zero
      despite an error condition being detected. In such cases the new check
      will not detect the error and the function will continue as before.
      A possibly more troublesome change is in raising an error at all; if
      users expected only a warning when an error has occurred they may now
      have anhandled exception in their code which would cause a halt to
      execution.

--- mcstasscript/tests/test_Instr.py
+++ mcstasscript/tests/test_Instr.py
@@ -1702,6 +1702,11 @@ class TestMcStas_instr(unittest.TestCase):
         existing file so no error is thrown.
         """
 
+        # The mocked process needs to set a return-code of zero to avoid the process error checking
+        process_mock = unittest.mock.Mock()
+        process_mock.configure_mock(returncode=0)
+        mock_sub.return_value = process_mock
+
         THIS_DIR = os.path.dirname(os.path.abspath(__file__))
         executable_path = os.path.join(THIS_DIR, "dummy_mcstas")
 
@@ -1768,6 +1773,11 @@ class TestMcStas_instr(unittest.TestCase):
         existing file so no error is thrown.
         """
 
+        # The mocked process needs to set a return-code of zero to avoid the process error checking
+        process_mock = unittest.mock.Mock()
+        process_mock.configure_mock(returncode=0)
+        mock_sub.return_value = process_mock
+
         THIS_DIR = os.path.dirname(os.path.abspath(__file__))
         executable_path = os.path.join(THIS_DIR, "dummy_mcstas")
 
@@ -1813,6 +1823,11 @@ class TestMcStas_instr(unittest.TestCase):
         the output_path is set to a name that does not correspond to a
         existing file so no error is thrown.
         """
+
+        # The mocked process needs to set a return-code of zero to avoid the process error checking
+        process_mock = unittest.mock.Mock()
+        process_mock.configure_mock(returncode=0)
+        mock_sub.return_value = process_mock
 
         THIS_DIR = os.path.dirname(os.path.abspath(__file__))
         executable_path = os.path.join(THIS_DIR, "dummy_mcstas")
@@ -1867,6 +1882,11 @@ class TestMcStas_instr(unittest.TestCase):
         parameters in run_full_instrument.
         """
 
+        # The mocked process needs to set a return-code of zero to avoid the process error checking
+        process_mock = unittest.mock.Mock()
+        process_mock.configure_mock(returncode=0)
+        mock_sub.return_value = process_mock
+
         THIS_DIR = os.path.dirname(os.path.abspath(__file__))
         executable_path = os.path.join(THIS_DIR, "dummy_mcstas")
 
@@ -1920,6 +1940,11 @@ class TestMcStas_instr(unittest.TestCase):
         the output_path is set to a name that does not correspond to a
         existing file so no error is thrown.
         """
+
+        # The mocked process needs to set a return-code of zero to avoid the process error checking
+        process_mock = unittest.mock.Mock()
+        process_mock.configure_mock(returncode=0)
+        mock_sub.return_value = process_mock
 
         THIS_DIR = os.path.dirname(os.path.abspath(__file__))
         executable_path = os.path.join(THIS_DIR, "dummy_mcstas")
